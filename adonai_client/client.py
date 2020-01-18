35a611137ae64a9ffa6943b78a9652c5f3d5bb3f
Aleksander Lavrov <lavrov@selectel.ru>
2020-01-19 01:32:09 +0300

    fixes

--- adonai_client/client.py
+++ adonai_client/client.py
@@ -97,7 +97,7 @@ class AdonaiClient:
         return self._gql_endpoint(operation)
 
     def get_query(
-        self, query_type: QueryType, query_name: str, exclude_fields: tuple = ()
+        self, query_type: QueryType, query_name: str
     ) -> Operation:
         """
         :param query_type: query or mutation
@@ -131,8 +131,5 @@ class AdonaiClient:
             )
 
         query_selection = query_selection()
-
-        for field in exclude_fields:
-            query_selection.__fields__(**{field: False})
 
         return query
