e41e041156d192fb2de8d23ce7808e13cd8ae541
Aleksander Lavrov <lavrov@selectel.ru>
2020-01-19 01:13:19 +0300

    typing

--- adonai_client/client.py
+++ adonai_client/client.py
@@ -20,6 +20,26 @@ class AdonaiClient:
         api_route: str = "/",
         token_prefix: str = "JWT ",
     ):
+        """
+        :param server_root_endpoint: url to server, example http://localhost:5000
+        :type server_root_endpoint: str
+
+        :param username: user login name
+        :type username: str
+
+        :param password: user password
+        :type password: str
+
+        :param auth_route: auth route on server, defaults to "/auth"
+        :type auth_route: str, optional
+
+        :param api_route: api route on server, defaults to "/"
+        :type api_route: str, optional
+
+        :param token_prefix: bearer token prefix with space after, defaults to "JWT "
+        :type token_prefix: str, optional
+        """
+    
         self._username = username
         self._password = password
 
@@ -33,7 +53,13 @@ class AdonaiClient:
             base_headers={"Authorization": f"{token_prefix}{self._token}"},
         )
 
-    def _get_auth_token(self):
+    def _get_auth_token(self) -> str:
+        """
+        :raises AdonaiClientException: raises on server response status not OK (200)
+        :return: JWT token by user auth data
+        :rtype: str
+        """
+        
         response = httpx.post(
             self._auth_endpoint,
             json={"username": self._username, "password": self._password},
@@ -48,18 +74,44 @@ class AdonaiClient:
 
     @property
     def query(self) -> schema.query_type:
+        """        
+        :return: operation based on query
+        :rtype: schema.query_type
+        """
         return Operation(schema.query_type)
 
     @property
     def mutation(self) -> schema.query_type:
+        """
+        :return: operation based on mutation
+        :rtype: schema.query_type
+        """
         return Operation(schema.mutation_type)
 
     def execute(self, operation: Selection):
+        """
+        :param operation: queyr or mutation operation
+        :type operation: Selection
+        :return: result of query
+        """
         return self._gql_endpoint(operation)
 
     def get_query(
         self, query_type: QueryType, query_name: str, exclude_fields: tuple = ()
-    ):
+    ) -> Operation:
+        """
+        :param query_type: query or mutation
+        :type query_type: QueryType
+        :param query_name: query or mutation name
+        :type query_name: str
+        :param exclude_fields: excluded fields from final query, defaults to ()
+        :type exclude_fields: tuple, optional
+        :raises AdonaiClientException: unexpected query type
+        :raises AdonaiClientException: unexpected query name
+        :return: query
+        :rtype: Operation
+        """
+
         query = None
 
         if query_type == QueryType.query:
